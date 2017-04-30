0e8f3f1f141035474051b7ad15eef88ca041aec7
bmmors <bmmors@gmail.com>
2017-04-29 18:22:27 -0700

    Policies Written
    Basic code for policies and EA written. Got confused.

--- Delta.cpp
+++ Delta.cpp
@@ -11,12 +11,14 @@
 #include <cmath>
 #include <time.h>
 #include <fstream>
+#include "LY_NN.h"
+
 
 using namespace std;
 
 #define BMMRAND (double)rand()/RAND_MAX;
 
-////Simulation Setup////
+////----------Simulation Setup----------////
 class ship {
 public:
 	vector<double> x;
@@ -60,22 +62,105 @@ void ship::reset() {
 	init();
 }
 
-////End Simulation Setup////
+////----------End Simulation Setup----------////
+
+
+
+
+////----------Goal Setup----------////
+class goal {
+public:
+	///To be randomized later
+	vector<double> x;
+	vector<double> y;
+
+	void init();
+};
+
+void goal::init() {
+	x.push_back(900);
+	x.push_back(950);
+
+	y.push_back(900);
+	y.push_back(900);
+
+	//makes goal start at (900,900) and go to (950,900)
+}
+////----------End Goal Setup----------////
+
+
+
+////----------Policy Setup----------////
+class policy {
+public:
+	vector<double> weights;
+	double fitness;
+	double u;
+
+	void init(int num_weights);
+	void mutate();
+};
+
+void policy::init(int num_weights) {
+	for (int i = 0; i < num_weights; i++) {
+		int test = rand() % 2; //generate 0 or 1. Zero means + weight 1 means 0 weight
+		assert(test == 0 || test == 1);
+		if (test == 0) {
+			double w = BMMRAND
+			weights.push_back(w);
+		}
+		else if (test == 1) {
+			double w = BMMRAND;
+			w = w * -1;
+			weights.push_back(w);
+		}
+	}
+	assert(weights.size() == num_weights);
+}
+////----------End Policy Setup----------////
+
+
+
+
+////----------Evolutionary Algorithm Setup----------////
+class EA {
+public:
+	vector<policy> population;
+
+	void replicate();
+	void evaluate();
+	void downselect();
+};
+
+////----------End Evolutionary Algorithm----------////
+
+
+
 
 int main() {
 
 	///Simulation///
-	double u = 0; //set to zero for straight line movement
 	ship s;
 	s.init();
-	s.updatepos(u);
-	s.updatepos(u);
-	s.updatepos(u);
-	
-	for (int i = 0; i < s.x.size(); i++) {
-		cout << "x:\t" << s.x[i] << "\ty:\t" << s.y[i] << "\ttheta\t" << s.theta[i] << "\tomega\t" << s.omega[i] << endl;
-	}
+
+	//cout << "x:\t" << s.x[i] << "\ty:\t" << s.y[i] << "\ttheta\t" << s.theta[i] << "\tomega\t" << s.omega[i] << endl;
 	///End Simulation///
 
+	///Evolution Algorithm///
+	EA e;
+	int num_poly = 50;
+	int num_weights = 10; //to be determined using NN later
+
+	for (int i = 0; i < num_poly; i++) {
+		policy p;
+		p.init(num_weights);
+		e.population.push_back(p);
+	}
+	assert(e.population.size() == num_poly);
+	///End Evolutionary Algorithm///
+
+	///Start Full Sim with EA and NN///
+	int max_time = 1000; //max number of time for each simulation
+	///End Full Sim with EA and NN///
 	return 0;
 }
